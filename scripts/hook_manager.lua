044cc2c3f6072c60608af3b8f75e39de23c0a1f0
rebecca-butler <rebecca.erin.butler@gmail.com>
2025-05-30 18:38:19 -0700

    clean up, in a working state

--- scripts/hook_manager.lua
+++ scripts/hook_manager.lua
@@ -5,7 +5,6 @@ HookManager.activeHooks = {}
 
 -- Register a hook and remember the callback IDs
 function HookManager.Register(key, functionPath, callback)
-    -- HookManager.Unregister(key)
     local preId, postId = RegisterHook(functionPath, callback)
     HookManager.activeHooks[key] = {
         path = functionPath,
@@ -18,10 +17,7 @@ end
 function HookManager.Unregister(key)
     local hookData = HookManager.activeHooks[key]
     if hookData then
-        print("[MapCompletionMarkerMod] Unregistering: " .. tostring(hookData.path) .. " pre-hook" .. tostring(hookData.pre) .. " post-hook" .. tostring(hookData.post))
-        -- UnregisterHook() crashes after unregistering OnFadeToGameBeginEventReceived
         UnregisterHook(hookData.path, hookData.pre or 0, hookData.post or 0)
-        print("[MapCompletionMarkerMod] Unregister complete")
         HookManager.activeHooks[key] = nil
     end
 end
